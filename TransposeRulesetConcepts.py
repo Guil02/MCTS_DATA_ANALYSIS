59cbc9f021586df540af421303ff3b805ebc85eb
guil <g.bams@student.maastrichtuniversity.nl>
2023-10-11 17:57:58 +0200

    optimized some of the code

--- TransposeRulesetConcepts.py
+++ TransposeRulesetConcepts.py
@@ -4,11 +4,19 @@ import polars as pl
 # the goal of this file is to import the data/ludii-database-files/RulesetConcepts.csv file and manipulate
 # it such that the first column is every unique RulesetId and all the following columns are the concepts and their value
 
-def run():
+def run(conceptId=False):
     data = pl.read_csv('new_csv/RulesetConceptsImportant.csv', infer_schema_length=None)
-    data = data.select(['RulesetId', 'ConceptId', 'Value'])
+    concepts = pl.read_csv('data/ludii-database-files/Concepts.csv', infer_schema_length=None)
+    concepts = concepts.select(['Id', 'Name'])
+    data = data.join(concepts, left_on='ConceptId', right_on='Id', how='left')
+    data = data.select(['RulesetId', 'ConceptId', 'Value', 'Name'])
     data = data.sort('ConceptId')
-    data = data.pivot(index='RulesetId', columns='ConceptId', values='Value')
+    if conceptId:
+        data = data.select(['RulesetId', 'ConceptId', 'Value'])
+        data = data.pivot(index='RulesetId', columns='ConceptId', values='Value')
+    else:
+        data = data.select(['RulesetId', 'Name', 'Value'])
+        data = data.pivot(index='RulesetId', columns='Name', values='Value')
     data.write_csv('new_csv/RulesetConceptsSorted.csv')
 
 
--- main.py
+++ main.py
@@ -6,14 +6,20 @@ import remove_non_important_concepts
 from pathlib import Path
 
 print('Starting')
+run_consecutive = False
 if not Path.is_file(Path('new_csv/AllGames.csv')):
+    run_consecutive = True
     ReadGameData.run()
-if not Path.is_file(Path('new_csv/RulesetConceptsImportant.csv')):
+if not Path.is_file(Path('new_csv/RulesetConceptsImportant.csv')) or run_consecutive:
+    run_consecutive = True
     remove_non_important_concepts.run()
-if not Path.is_file(Path('new_csv/RulesetConceptsSorted.csv')):
-    TransposeRulesetConcepts.run()
-if not Path.is_file(Path('new_csv/AllGamesWithRulesetConcepts.csv')):
+if not Path.is_file(Path('new_csv/RulesetConceptsSorted.csv')) or run_consecutive:
+    run_consecutive = True
+    TransposeRulesetConcepts.run(False)
+if not Path.is_file(Path('new_csv/AllGamesWithRulesetConcepts.csv')) or run_consecutive:
+    run_consecutive = True
     joinFiles.run()
-if not Path.is_file(Path('new_csv/data_with_agents_split.csv')):
+if not Path.is_file(Path('new_csv/data_with_agents_split.csv')) or run_consecutive:
+    run_consecutive = True
     ModifyGameDataValues.run()
 print('Done')
