ac5b0ba0635b84ed6a3a72a83ff5d8116364acb4
StefanStefanov741 <tetijava@gmail.com>
2024-01-23 15:33:24 +0100

    -updated models to use new set

--- Stefan Phase3 Models/p3_naive.py
+++ Stefan Phase3 Models/p3_naive.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import cross_val_score, StratifiedKFold
-from sklearn.metrics import make_scorer, f1_score
+from sklearn.dummy import DummyClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import make_scorer, f1_score
 
 df = pd.read_csv('training_data/sets/components classification.csv')
 
@@ -10,10 +10,9 @@ df = pd.read_csv('training_data/sets/components classification.csv')
 df.replace({True: 1, False: 0}, inplace=True)
 
 #Remove rows with Random
-df = df[(df['Selection 1'] != 'Random') & (df['Selection 2'] != 'Random')]
+df = df[(df['Selection 1 - Random'] != 1) & (df['Selection 2 - Random'] != 1)]
 
 #Encode categorical variables
-df = pd.get_dummies(df, columns=['Selection 1', 'Selection 2', 'Play-out 1', 'Play-out 2'])
 label_encoder = LabelEncoder()
 df['Most common outcome for Agent 1'] = label_encoder.fit_transform(df['Most common outcome for Agent 1'])
 
@@ -23,20 +22,14 @@ y = df['Most common outcome for Agent 1']
 #Use StratifiedKFold
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-naive_classifier = DummyClassifier(strategy='most_frequent')
-
-#Get the most frequent class
-most_frequent_class_encoded = y.value_counts().idxmax()
-
-#Reverse the encoding to get the original label
-most_frequent_class_original = label_encoder.inverse_transform([most_frequent_class_encoded])[0]
+#Create a DummyClassifier that predicts the most frequent class
+dummy_classifier = DummyClassifier(strategy='most_frequent')
 
 #Perform 5-fold cross-validation
 f1_scorer = make_scorer(f1_score, average='macro')
-cross_val_results_accuracy = cross_val_score(naive_classifier, X, y, cv=cv, scoring='accuracy')
-cross_val_results_f1 = cross_val_score(naive_classifier, X, y, cv=cv, scoring=f1_scorer)
+cross_val_results_accuracy = cross_val_score(dummy_classifier, X, y, cv=cv, scoring='accuracy')
+cross_val_results_f1 = cross_val_score(dummy_classifier, X, y, cv=cv, scoring=f1_scorer)
 
 #Print results
-print(f"Most Frequent Class (Naive Classifier): {most_frequent_class_original}")
-print(f"Cross-validation Accuracy (Naive Classifier): {cross_val_results_accuracy.mean():.2f}")
-print(f"Cross-validation F1 Score (Naive Classifier): {cross_val_results_f1.mean():.2f}")
+print(f"Cross-validation Accuracy: {cross_val_results_accuracy.mean():.2f}")
+print(f"Cross-validation F1 Score: {cross_val_results_f1.mean():.2f}")
